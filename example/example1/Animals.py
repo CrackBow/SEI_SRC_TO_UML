159e6ab47f550149662bbdda7e1b3de470896d1c
BigTtheCSguy <118405066+BigTtheCSguy@users.noreply.github.com>
2022-12-06 00:40:19 -0600

    Add files via upload

---
+++ example/example1/Animals.py
@@ -0,0 +1,16 @@
+class Animal:
+	def __init__(self):
+		pass
+
+	def makeNoise():
+		pass
+
+class Dog(Animal):
+	def wagTail():
+		pass
+
+	def __somePrivateMethod():
+		pass
+
+class Cat(Animal):
+	pass
---
+++ example/example1/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+../../py2uml.py Test#2.py | dot -T png -o output.png
